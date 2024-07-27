9c746e3774eb35205902e5afe67520bdcc992b36
Ledchig <s9826059221@gmail.com>
2024-07-27 13:24:25 +0500

    init app and set configs

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Test for F5',
+  description: 'Made by Savonin Nikolay',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="ru">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
